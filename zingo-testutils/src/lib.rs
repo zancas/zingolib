c93d5fbf24f6bbc475abe5de1fe87e48ffa5f275
zancas <zancas@zingolabs.org>
2023-08-13 21:33:00 -0600

    make scenarios match benchmark interface

--- zingo-testutils/src/lib.rs
+++ zingo-testutils/src/lib.rs
@@ -746,14 +746,15 @@ pub mod scenarios {
                 .read()
                 .await
                 .clone();
+            // Delete the client after getting the capability.
             // Extract the orchard fvk
             let [o_fvk, s_fvk, t_fvk] = build_fvks_from_wallet_capability(&wallet_capability);
-            let (keyowning, _) =
+            let (viewing_client, _) =
                 build_fvk_client_and_capability(&[&o_fvk, &s_fvk, &t_fvk], &zingo_config).await;
             (
                 sb.regtest_manager,
                 sb.child_process_handler.unwrap(),
-                keyowning,
+                viewing_client,
                 original_recipient,
             )
         }
