369d88b9ed26e0eed86f17bc251a6effd432d1e8
zancas <zancas@zingolabs.org>
2024-06-10 16:09:28 -0600

    rename spendable_balance to shielded_spendable_balance

--- zingolib/src/commands.rs
+++ zingolib/src/commands.rs
@@ -631,7 +631,7 @@ impl Command for SpendableBalanceCommand {
             }
         };
         RT.block_on(async move {
-            match lightclient.spendable_balance(address).await {
+            match lightclient.shielded_spendable_balance(address).await {
                 Ok(bal) => {
                     object! {
                         "balance" => bal.into_u64(),
